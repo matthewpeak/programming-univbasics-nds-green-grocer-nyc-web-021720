dba5676cc96c577714585544ac33fb47674244c4
Matthew Peak <matthewpeak@gmail.com>
2020-02-13 18:41:53 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,7 +41,6 @@ def apply_coupons(cart, coupons)
   coupons_index = 0
 
   while coupons_index < coupons.size do
-    current_coupon = coupons[coupons_index]
     applicable_for_discount = find_item_by_name_in_collection( current_coupon[:item], cart )
       if ( applicable_for_discount[:count] / current_coupon[:num] >= 1 )
         cart.push( {:item => "#{current_coupon[:item]} W/COUPON",
