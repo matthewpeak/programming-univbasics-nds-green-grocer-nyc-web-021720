abbad919002ec6ab1008bcb1ea313a437bb0f701
Matthew Peak <matthewpeak@gmail.com>
2020-02-13 18:43:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -41,14 +41,14 @@ def apply_coupons(cart, coupons)
   coupons_index = 0
 
   while coupons_index < coupons.size do
-    applicable_for_discount = find_item_by_name_in_collection( current_coupon[:item], cart )
-      if ( applicable_for_discount[:count] / current_coupon[:num] >= 1 )
-        cart.push( {:item => "#{current_coupon[:item]} W/COUPON",
-                    :price => (current_coupon[:cost] / current_coupon[:num]).round(2),
+    applicable_for_discount = find_item_by_name_in_collection( coupons[coupons_index][:item], cart )
+      if ( applicable_for_discount[:count] / coupons[coupons_index][:num] >= 1 )
+        cart.push( {:item => "#{coupons[coupons_index][:item]} W/COUPON",
+                    :price => (coupons[coupons_index][:cost] / coupons[coupons_index][:num]).round(2),
                     :clearance => applicable_for_discount[:clearance],
-                    :count => applicable_for_discount[:count] - ( applicable_for_discount[:count] % current_coupon[:num])})
+                    :count => applicable_for_discount[:count] - ( applicable_for_discount[:count] % coupons[coupons_index][:num])})
 
-        applicable_for_discount[:count] %= current_coupon[:num]
+        applicable_for_discount[:count] %= coupons[coupons_index][:num]
       end
     coupons_index += 1
   end
